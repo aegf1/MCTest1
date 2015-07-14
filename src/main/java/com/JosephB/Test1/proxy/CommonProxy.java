305d329cae62c2c958e402c85080afb09431df88
aegf1 <aegf1@hotmail.co.uk>
2015-07-14 15:09:03 +0100

    added structures: proxies, reference+utility, config, logging

---
+++ src/main/java/com/JosephB/Test1/proxy/CommonProxy.java
@@ -0,0 +1,6 @@
+package com.JosephB.Test1.proxy;
+
+public abstract class CommonProxy implements IProxy {
+
+	
+}
---
+++ src/main/java/com/JosephB/Test1/proxy/ServerProxy.java
@@ -0,0 +1,6 @@
+package com.JosephB.Test1.proxy;
+
+public class ServerProxy extends CommonProxy {
+
+	
+}
