5f799812b496a4d96adb95c2f4a9774c192717b6
Joseph B <aegf1@hotmail.co.uk>
2015-07-23 16:32:35 +0100

    Made magnet blocks orientable (took a lot of bugfixing). They now extend BlockRotatedPillerTest1, which can define any such block with top, bottem and 2 sides. Also added textures and bound them to sides. But, orientations seem to be wrong atm so need to be fixed.

--- src/main/java/com/JosephB/Test1/block/BlockMagnet.java
+++ src/main/java/com/JosephB/Test1/block/BlockMagnet.java
@@ -1,8 +1,20 @@
 package com.josephb.test1.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumWorldBlockLayer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockMagnet extends BlockTest1
+public class BlockMagnet extends BlockRotatedPillerTest1
 {
 	private final String name = "Magnet";
 	
@@ -10,13 +22,15 @@ public class BlockMagnet extends BlockTest1
 	{
 		super(Material.iron);
 		this.setUnlocalizedName(name);
-		
+		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
 	}
+	
 	
 	@Override
 	public String getName()
 	{
 		return name;
 	}
-
+	
+	
 }
---
+++ src/main/java/com/JosephB/Test1/block/BlockRotatedPillerTest1.java
@@ -0,0 +1,128 @@
+package com.josephb.test1.block;
+
+import com.josephb.test1.creativetab.CreativeTabTest1;
+import com.josephb.test1.reference.Reference;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumWorldBlockLayer;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BlockRotatedPillerTest1 extends BlockTest1
+{
+	public static final PropertyDirection FACING = PropertyDirection.create("facing");
+	private static final String __OBFID = "CL_00000302";
+	
+	/**
+	 * Constructor that lets you define block material
+	 * @param material
+	 */
+	public BlockRotatedPillerTest1(Material material)
+	{
+		super(material);
+		this.setCreativeTab(CreativeTabTest1.Test1_Tab);
+	}
+
+	/**
+	 * Constructor that sets block material to iron
+	 */
+	public BlockRotatedPillerTest1()
+	{
+		this(Material.iron);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public EnumWorldBlockLayer getBlockLayer()
+	{
+		return EnumWorldBlockLayer.SOLID;
+	}
+	
+	@Override
+	public boolean isOpaqueCube()
+	{
+		return true;
+	}
+	
+	@Override
+	public boolean isFullCube()
+	{
+		return true;
+	}
+	
+	@Override
+	public int getRenderType()
+	{
+		return 3;
+	}
+
+	public static EnumFacing getFacing(int meta)
+    {
+        int j = meta & 7;
+        return j > 5 ? null : EnumFacing.getFront(j);
+    }
+
+    /**
+     * Convert the given metadata into a BlockState for this Block
+     */
+    public IBlockState getStateFromMeta(int meta)
+    {
+    	return this.getDefaultState().withProperty(FACING, getFacing(meta));
+    }
+
+    /**
+     * Convert the BlockState into the correct metadata value
+     */
+    public int getMetaFromState(IBlockState state)
+    {
+    	byte b0 = 0;
+        int i = b0 | ((EnumFacing)state.getValue(FACING)).getIndex();
+		return i;
+    }
+
+    protected BlockState createBlockState()
+    {
+        return new BlockState(this, new IProperty[] {FACING});
+    }
+
+    protected ItemStack createStackedBlock(IBlockState state)
+    {
+        return new ItemStack(Item.getItemFromBlock(this), 1, 0);
+    }
+
+    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+    	return this.getDefaultState().withProperty(FACING, getFacingFromEntity(worldIn, pos, placer));
+    }
+    
+    public static EnumFacing getFacingFromEntity(World worldIn, BlockPos clickedBlock, EntityLivingBase entityIn)
+    {
+        if (MathHelper.abs((float)entityIn.posX - (float)clickedBlock.getX()) < 2.0F && MathHelper.abs((float)entityIn.posZ - (float)clickedBlock.getZ()) < 2.0F)
+        {
+            double d0 = entityIn.posY + (double)entityIn.getEyeHeight();
+
+            if (d0 - (double)clickedBlock.getY() > 2.0D)
+            {
+                return EnumFacing.UP;
+            }
+
+            if ((double)clickedBlock.getY() - d0 > 0.0D)
+            {
+                return EnumFacing.DOWN;
+            }
+        }
+
+        return entityIn.getHorizontalFacing().getOpposite();
+    }
+}
